7949205f1612c996eeb6f1fd5b411bb0e45edcd8
taadams1 <rainedoescode@gmail.com>
2022-06-11 12:53:33 -0500

    Initial research and framework for stages

---
+++ client/scripts/stages.js
@@ -0,0 +1,35 @@
+/**
+ *  .-----------------------.
+ *  |  Game stages (w.i.p)  |
+ *  |_______________________|
+ */
+
+module.exports = {
+  // Stage 1, no concept of numbers or counting. Each number is a single "thing".
+  // Quickly moves on to basic grouping with finger counting
+  things: (num) => {
+    // display "thing"
+  },
+
+  // Stage 2, early grouping of things using tallies and finger counting.
+
+  // Tokens, addition of subtraction
+
+  // Sumerian clay tablets, rise of writing
+
+  // Rise of measurement in Egypt - Cubits
+  // Egyptian make symbols for different numbers
+
+  // THEORETICAL MATH
+
+  // Pythagoras - odd and even numbers
+  // Archimedes - many contributions to math ("math games") ("Archimedes Screw")
+
+  // Death of Archimedes - end of Greek Math's "Golden Age"
+
+  // Roman Numerals - counting board
+
+  // AGE OF ZERO
+
+  // Indians invent Zero, create individual symbols for 1-9
+}
