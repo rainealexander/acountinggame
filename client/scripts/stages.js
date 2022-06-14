864d5db273cf82e024c1bed9eb16d47520a7b6ba
taadams1 <rainedoescode@gmail.com>
2022-06-14 13:30:12 -0500

    Progress on research and stages

--- client/scripts/stages.js
+++ client/scripts/stages.js
@@ -1,19 +1,49 @@
-/**
- *  .-----------------------.
- *  |  Game stages (w.i.p)  |
- *  |_______________________|
- */
 
-module.exports = {
+//  .-----------------------.
+//  |  Game stages (w.i.p)  |
+//  |_______________________|
+
+
+// will need function to "dispatch" current stage
+
+// testing data structure for stages and their requirements
+const stages = {
+  stage1: {
+    requirements: 0,
+    methods: [
+      {
+        things: {
+          requirements: 0
+        }
+      }
+    ]
+  },
+  stage2: [fingers, tallies_single, tallies_grouped]
+}
+
+const stageFunctions = {
   // Stage 1, no concept of numbers or counting. Each number is a single "thing".
   // Quickly moves on to basic grouping with finger counting
+  // # 1-10?
   things: (num) => {
     // display "thing"
   },
 
   // Stage 2, early grouping of things using tallies and finger counting.
+  fingers: () => {
+    // can only count up to 10
+  },
 
-  // Tokens, addition of subtraction
+  tallies_single: () => {
+    // can count to any number, but no groupings
+  },
+
+  tallies_grouped: () => {
+    // grouped tallies - easier to read
+  },
+
+  // Tokens, "addition" of subtraction
+  // (trading mini game?)
 
   // Sumerian clay tablets, rise of writing
 
@@ -29,7 +59,7 @@ module.exports = {
 
   // Roman Numerals - counting board
 
-  // AGE OF ZERO
+  // 000~ AGE OF ZERO ~000
 
   // Indians invent Zero, create individual symbols for 1-9
 }
